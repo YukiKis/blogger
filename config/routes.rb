c2294e84e0cff50d265df1befa6fc3335499a018
EC2 Default User <ec2-user@ip-172-31-42-86.ec2.internal>
2021-02-17 14:43:06 +0000

    make login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "homes#top"
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+  resources :users, except: [:new, :create]
   get 'homes/top'
   get 'homes/about', as: :about
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
